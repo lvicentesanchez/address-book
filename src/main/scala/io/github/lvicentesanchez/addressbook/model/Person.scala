f21a0b82d3834e8cb708ea83e2fa12f1aed5f7e3
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-09-07 20:08:34 +0100

    * fix person tests

--- src/main/scala/io/github/lvicentesanchez/addressbook/model/Person.scala
+++ src/main/scala/io/github/lvicentesanchez/addressbook/model/Person.scala
@@ -1,7 +1,20 @@
 package io.github.lvicentesanchez.addressbook.model
 
+import scala.util.control.Exception._
+
 case class Person(name: Name, gender: Gender, dob: DateOfBirth)
 
 object Person {
-  def unapply(string: String): Option[Person] = None
+  def unapply(string: String): Option[Person] =
+    allCatch.opt(
+      string.split(", ").toIterator.take(3).to[List]
+    ).flatMap(_ match {
+      case fst :: snd :: thr :: Nil =>
+        for {
+          name <- Name.unapply(fst)
+          gender <- Gender.unapply(snd)
+          dob <- DateOfBirth.unapply(thr)
+        } yield Person(name, gender, dob)
+      case _ => None
+    })
 }
--- src/test/scala/io/github/lvicentesanchez/addressbook/model/PersonSpec.scala
+++ src/test/scala/io/github/lvicentesanchez/addressbook/model/PersonSpec.scala
@@ -13,14 +13,14 @@ class PersonSpec extends Spec {
    """
 
   def notValidPersonString = {
-    val string: String = "Luis Vicente, Male, 79/02/16"
-    val gender: Option[Name] = Name.unapply(string)
+    val string: String = "Luis Vicente, Male"
+    val gender: Option[Person] = Person.unapply(string)
     gender must beNone
   }
 
   def validParsonString = {
     val string: String = "Luis Vicente, Male, 16/02/79"
-    val gender: Option[Name] = Name.unapply(string)
+    val gender: Option[Person] = Person.unapply(string)
     gender must beSome(Person(Name("Luis", "Vicente"), Male, DateOfBirth(LocalDate.of(1979, 2, 16))))
   }
 }
