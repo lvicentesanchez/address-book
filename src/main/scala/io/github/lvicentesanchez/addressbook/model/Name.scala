564290895a65ce132a3b963f9d93aebbfe6d5a35
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-09-07 19:49:57 +0100

    * fix name tests

--- src/main/scala/io/github/lvicentesanchez/addressbook/model/Name.scala
+++ src/main/scala/io/github/lvicentesanchez/addressbook/model/Name.scala
@@ -1,7 +1,15 @@
 package io.github.lvicentesanchez.addressbook.model
 
+import scala.util.control.Exception._
+
 case class Name(first: String, surname: String)
 
 object Name {
-  def unapply(string: String): Option[Name] = None
+  val Regexp = """^(\w*) (\w*)$""".r
+
+  def unapply(string: String): Option[Name] =
+    allCatch.opt {
+      val Regexp(first, surname) = string
+      Name(first, surname)
+    }
 }
