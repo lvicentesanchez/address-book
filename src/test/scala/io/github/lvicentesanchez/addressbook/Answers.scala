e1bdf93f8473df39624eaca6eb2139995179463b
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-09-07 22:57:22 +0100

    * answer questions

--- src/test/scala/io/github/lvicentesanchez/addressbook/Answers.scala
+++ src/test/scala/io/github/lvicentesanchez/addressbook/Answers.scala
@@ -1,18 +1,27 @@
 package io.github.lvicentesanchez.addressbook
 
-import io.github.lvicentesanchez.addressbook.model.Person
+import java.time.temporal.{ ChronoUnit, Temporal }
+import java.time.{ Period, LocalDate }
+
+import io.github.lvicentesanchez.addressbook.data.{ Leaf, Tree }
+import io.github.lvicentesanchez.addressbook.data.tree._
+import io.github.lvicentesanchez.addressbook.model._
 import org.specs2.Spec
 import org.specs2.execute.{ Result, AsResult }
 import org.specs2.specification.ForEach
 
 import scala.io.Source
 
-class Answers extends Spec with ForEach[Iterator[Person]] {
+class Answers extends Spec with ForEach[Tree[Person]] {
 
-  override protected def foreach[R](f: (Iterator[Person]) => R)(implicit AR: AsResult[R]): Result = {
+  override protected def foreach[R](f: (Tree[Person]) => R)(implicit AR: AsResult[R]): Result = {
     val stream = this.getClass.getResourceAsStream("/AddressBook")
     val source = Source.fromInputStream(stream)
-    val input = source.getLines().flatMap(Person.unapply(_))
+    val input =
+      source.
+        getLines().
+        flatMap(Person.unapply(_)).
+        foldLeft[Tree[Person]](Leaf)((acc, a) => Tree.add(a, acc))
     val result = f(input)
     source.close()
     stream.close()
@@ -24,19 +33,37 @@ class Answers extends Spec with ForEach[Iterator[Person]] {
 
       How many males are in the address book? 3                            ${answerHowManyMales _}
       Who is the oldest person in the address book? Wes Jackson (14/08/74) ${answerOldestPerson _}
-      How many days older is Bill than Paul? 2863                          ${answerHowMayDaysBetweenBillAndPaul _}
+      How many days older is Bill than Paul? 2862                          ${answerHowMayDaysBetweenBillAndPaul _}
   """
 
-  def answerHowManyMales(input: Iterator[Person]) = {
-    input.foreach(println)
-    false
+  def answerHowManyMales(input: Tree[Person]) = {
+    val answer = howManyByCondition(input, isMale)
+    val expected = 3
+
+    answer must_== expected
+  }
+  def answerOldestPerson(input: Tree[Person]) = {
+    val answer = input.min
+    val expected = Person(Name("Wes", "Jackson"), Male, DateOfBirth(LocalDate.of(1974, 8, 14)))
+
+    answer must beSome(expected)
   }
 
-  def answerOldestPerson(input: Iterator[Person]) = {
-    false
+  def answerHowMayDaysBetweenBillAndPaul(input: Tree[Person]) = {
+    val answer = for {
+      bill <- input.findFirst(_.name.first.toUpperCase == "BILL")
+      paul <- input.findFirst(_.name.first.toUpperCase == "PAUL")
+    } yield daysBetween(bill.dob.date, paul.dob.date)
+    val expected = 2862L
+
+    answer must beSome(expected)
   }
 
-  def answerHowMayDaysBetweenBillAndPaul(input: Iterator[Person]) = {
-    false
-  }
+  def daysBetween(startInclusive: Temporal, endExclusive: Temporal): Long =
+    ChronoUnit.DAYS.between(startInclusive, endExclusive)
+
+  def howManyByCondition(input: Tree[Person], f: Person => Boolean): Int =
+    input.fold(0)((acc, a) => if (f(a)) acc + 1 else acc)
+
+  val isMale: Person => Boolean = _.gender == Male
 }
