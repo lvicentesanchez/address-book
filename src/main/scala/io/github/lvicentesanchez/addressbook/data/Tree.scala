8857223b60d04ce6a70b12a80f3f1c44729a668a
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-09-07 23:54:57 +0100

    * add RB-Tree implementation

--- src/main/scala/io/github/lvicentesanchez/addressbook/data/Tree.scala
+++ src/main/scala/io/github/lvicentesanchez/addressbook/data/Tree.scala
@@ -1,17 +1,55 @@
 package io.github.lvicentesanchez.addressbook.data
 
+/**
+ * Red-Black Tree implementation inspired by:
+ *   https://github.com/vkostyukov/scalacaster/blob/master/src/tree/RBTree.scala
+ */
+
+sealed trait Color
+
+case object Red extends Color
+case object Black extends Color
+
 sealed trait Tree[+A]
 
 case object Leaf extends Tree[Nothing]
 
-case class Branch[A](value: A, left: Tree[A], right: Tree[A]) extends Tree[A]
+case class Branch[A](value: A, color: Color, left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  def add[A](element: A, tree: Tree[A])(implicit O: Ordering[A]): Tree[A] = tree match {
-    case Leaf => Branch(element, Leaf, Leaf)
-    case Branch(value, left, right) if O.compare(element, value) == -1 => Branch(value, add(element, left), right)
-    case Branch(value, left, right) if O.compare(element, value) == 1 => Branch(value, left, add(element, right))
-    case other => other
+  def add[A: Ordering](element: A, tree: Tree[A])(implicit O: Ordering[A]): Tree[A] = {
+    def balanceAdd(x: A, rest: Tree[A]): Tree[A] = rest match {
+      case Leaf =>
+        Branch(x, Red, Leaf, Leaf)
+      case Branch(value, color, left, right) if O.compare(element, value) < 0 =>
+        balanceLeft(color, value, balanceAdd(x, left), right)
+      case Branch(value, color, left, right) if O.compare(element, value) > 0 =>
+        balanceRight(color, value, left, balanceAdd(x, right))
+      case other => other
+    }
+
+    def balanceLeft(color: Color, x: A, left: Tree[A], right: Tree[A]): Tree[A] = (color, left, right) match {
+      case (Black, Branch(y, Red, Branch(z, Red, lll, llr), lr), r) =>
+        Branch(y, Red, Branch(z, Black, lll, llr), Branch(x, Black, lr, r))
+      case (Black, Branch(z, Red, ll, Branch(y, Red, lrl, lrr)), r) =>
+        Branch(y, Red, Branch(z, Black, ll, lrl), Branch(x, Black, lrr, r))
+      case _ =>
+        Branch(x, color, left, right)
+    }
+
+    def balanceRight(color: Color, x: A, left: Tree[A], right: Tree[A]): Tree[A] = (color, left, right) match {
+      case (Black, l, Branch(y, Red, rl, Branch(z, Red, rrl, rrr))) =>
+        Branch(y, Red, Branch(x, Black, l, rl), Branch(z, Black, rrl, rrr))
+      case (Black, l, Branch(z, Red, Branch(y, Red, rll, rlr), rr)) =>
+        Branch(y, Red, Branch(x, Black, l, rll), Branch(z, Black, rlr, rr))
+      case _ =>
+        Branch(x, color, left, right)
+    }
+
+    balanceAdd(element, tree) match {
+      case Leaf => Leaf
+      case Branch(value, _, left, right) => Branch(value, Black, left, right)
+    }
   }
 
   def fold[A, B](value: A, tree: Tree[B])(f: (A, B) => A): A =
